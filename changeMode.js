f15af13f84d18a521b29a717ed8365bfabe0c963
rinky2022 <rinkyedulearn@gmail.com>
2022-02-21 10:29:42 -0800

    updated

--- changeMode.js
+++ changeMode.js
@@ -1,5 +1,7 @@
 function toggleMode()
 {
-    var mode = document.body; 
+    var mode = document.body;
+    var headermode =document.getElementById("header"); 
     mode.classList.toggle("darkMode");
+    headermode.classList.toggle("darkMode");
 }
